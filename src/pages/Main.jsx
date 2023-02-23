316d96b8f4112e6cafde75f256952074a13d4b82
hkw <hwibalyu@naver.com>
2023-02-23 23:30:34 +0900

    google trend page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { MdSend } from "react-icons/md";
+
 import GoogleTrends from "../components/GoogleTrends";
 
 export default function Main() {
   return (
-    <div className="flex flex-col items-center h-[2000px]">
+    <div className="flex flex-col items-center h-[1400px] gap-20">
       <div className="mt-48 text-4xl font-bold">
         Discover Trends Before They're Trending in Korea
       </div>
@@ -13,8 +15,18 @@ export default function Main() {
         <GoogleTrends keyword="루닉" />
         <GoogleTrends keyword="릴스" />
         <GoogleTrends keyword="수성의 마녀" />
-        {/* <GoogleTrends keyword="인게이지" />
-        <GoogleTrends keyword="트위치 1080" /> */}
+      </div>
+      <div className="flex w-12 gap-2 mt-10 lg:w-1/2">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="새로운 트렌드에 귀를 기울일 E-mail을 입력해주세요."
+          className="w-full font-bold text-center rounded-md bg-slate-100 h-14 focus:outline-sky-200 focus:outline-1"
+        />
+        <button className="px-10 text-xl font-semibold text-white rounded bg-cyan-700 hover:bg-cyan-600">
+          <MdSend />
+        </button>
       </div>
     </div>
   );
